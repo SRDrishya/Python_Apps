3194d41400fa99bd707da366065f0174f40b3be7
Drishya <sr.drishya@hotmail.com>
2026-07-08 16:46:17 +0530

    Add .env file for openweather key

--- Weather_App/main.py
+++ Weather_App/main.py
@@ -1,12 +1,8 @@
-def main():
-    print("Welcome to the Weather App!")
-    city = input("Enter the name of the city: ")
-    
-    if not city:
-        print("Please enter a valid city name.")
-        return
-    print(f"Fetching weather data for {city}...")   
+import requests
 
+def main():
+    print("Requests imported successfully. You can now use the requests library to make HTTP requests.")
+    
 
 if __name__ == "__main__":
     main()
